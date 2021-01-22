1da7cb117f789a89c0d6b2c5a0a102999647b8f0
D'yara Williams <dyara.williams@gmail.com>
2021-01-22 14:44:41 -0500

    update day 5 main.js

--- 05-Flex-Panel-Gallery/js/main.js
+++ 05-Flex-Panel-Gallery/js/main.js
@@ -1,8 +1,6 @@
 const panels = document.querySelectorAll(".panel");
-panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
-panels.forEach((panel) =>
-  panel.addEventListener("transitionend", toggleActive)
-);
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
 
 function toggleOpen() {
   this.classList.toggle("open");
